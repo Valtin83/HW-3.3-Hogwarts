4d0ab9c152221ac08fc551bc063a39fc7cc20067
zhe-v <zhe-valentin@mail.ru>
2025-02-26 22:12:56 +0700

    4.2 SQL Управление схемами баз данных

---
+++ Hogwarts/scripts423.sql
@@ -0,0 +1,8 @@
+SELECT student.name, student.age, faculty.faculty_name
+FROM Student student
+JOIN Faculty faculty ON student.faculty_id = faculty.faculty_id;
+
+SELECT student.name, student.age, faculty.faculty_name
+FROM Student student
+JOIN Faculty faculty ON student.faculty_id = faculty.faculty_id
+WHERE student.has_driving_license = TRUE;
